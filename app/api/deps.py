504e11855ea141611bcab28d3dfe50ccea839334
GustavoLino <gustavoaraujoln728@gmail.com>
2025-12-19 10:56:54 -0300

    feat: db connection and adding gitignore

---
+++ app/api/deps.py
@@ -0,0 +1,7 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.db import get_db_session
+
+
+DbSession = Annotated[AsyncSession, Depends(get_db_session)]
